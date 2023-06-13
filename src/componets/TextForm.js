ca25b8df0fb9c3a188ef99422bbd50f6b0fdb6c3
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-06-13 18:40:14 +0530

    final commit

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -63,7 +63,7 @@ export default function TextForm(props) {
 
     <div className="container my-3" style={{color:props.mode==='dark'?'white':'#161740e0'}}>
       <h2>Your text Summary</h2>
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} <b>words</b> ,{text.length} <b>charachters</b></p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} <b>words</b> ,{text.length} <b>charachters</b></p>
       <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
       <h5>Preview</h5>
       <p>{text.length>0?text:"Nothing to preview"}</p>
